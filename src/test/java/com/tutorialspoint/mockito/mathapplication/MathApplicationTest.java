beeff053b3928f98d8d52d69847c116c2232b569
Fanon Jupkwo <jupsfan@gmail.com>
2022-09-23 19:02:39 +0100

    timeouts

--- src/test/java/com/tutorialspoint/mockito/mathapplication/MathApplicationTest.java
+++ src/test/java/com/tutorialspoint/mockito/mathapplication/MathApplicationTest.java
@@ -1,7 +1,9 @@
 package com.tutorialspoint.mockito.mathapplication;
 
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -25,15 +27,25 @@ public class MathApplicationTest {
     }
 
     @Test
-    public void testAdd() {
+    public void testAddAndSubtract() {
 
-	// Given
-	given(calcService.add(20.0, 10.0)).willReturn(30.0);
+	// add the behavior to add numbers
+	when(calcService.add(20.0, 10.0)).thenReturn(30.0);
 
-	// when
-	double result = calcService.add(20.0, 10.0);
+	// subtract the behavior to subtract numbers
+	when(calcService.subtract(20.0, 10.0)).thenReturn(10.0);
 
-	// then
-	Assert.assertEquals(result, 30.0, 0);
+	// test the subtract functionality
+	Assert.assertEquals(mathApplication.subtract(20.0, 10.0), 10.0, 0);
+
+	// test the add functionality
+	Assert.assertEquals(mathApplication.add(20.0, 10.0), 30.0, 0);
+
+	// verify call to add method to be completed within 100 ms
+	verify(calcService, timeout(100)).add(20.0, 10.0);
+
+	// invocation count can be added to ensure multiple invocations
+	// can be checked within given timeframe
+	verify(calcService, timeout(100).times(1)).subtract(20.0, 10.0);
     }
 }
