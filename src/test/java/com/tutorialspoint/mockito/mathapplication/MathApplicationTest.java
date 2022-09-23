08f7df0b40af03387d677d3317fdf1162a760979
Fanon Jupkwo <jupsfan@gmail.com>
2022-09-23 16:22:32 +0100

    create mock

--- src/test/java/com/tutorialspoint/mockito/mathapplication/MathApplicationTest.java
+++ src/test/java/com/tutorialspoint/mockito/mathapplication/MathApplicationTest.java
@@ -1,32 +1,47 @@
 package com.tutorialspoint.mockito.mathapplication;
 
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 //@RunWith attaches a runner with the test class to initialize the test data
 @RunWith(MockitoJUnitRunner.class)
 public class MathApplicationTest {
 
-//@InjectMocks annotation is used to create and inject the mock object
-    @InjectMocks
-    MathApplication mathApplication = new MathApplication();
+    private MathApplication mathApplication;
+    private CalculatorService calcService;
 
-//@Mock annotation is used to create the mock object to be injected
-    @Mock
-    CalculatorService calcService;
+    @Before
+    public void setUp() {
+	mathApplication = new MathApplication();
+	calcService = mock(CalculatorService.class);
+	mathApplication.setCalculatorService(calcService);
+    }
 
-    @Test(expected = RuntimeException.class)
-    public void testAdd() {
-	// add the behavior to throw exception
-	doThrow(new RuntimeException("Add operation not implemented")).when(calcService).add(10.0, 20.0);
+    @Test
+    public void testAddAndSubtract() {
+
+	// add the behavior to add numbers
+	when(calcService.add(20.0, 10.0)).thenReturn(30.0);
+
+	// subtract the behavior to subtract numbers
+	when(calcService.subtract(20.0, 10.0)).thenReturn(10.0);
+
+	// test the subtract functionality
+	Assert.assertEquals(mathApplication.subtract(20.0, 10.0), 10.0, 0);
 
 	// test the add functionality
-	Assert.assertEquals(mathApplication.add(10.0, 20.0), 30.0, 0);
+	Assert.assertEquals(mathApplication.add(20.0, 10.0), 30.0, 0);
+
+	// verify call to calcService is made or not
+
+	verify(calcService).subtract(20.0, 10.0);
+	verify(calcService).add(20.0, 10.0);
     }
 }
