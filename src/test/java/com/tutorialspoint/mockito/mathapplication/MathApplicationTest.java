3cdc1a6b9ad6f84cf4a0bf2d8e6fc0681aaa54aa
Fanon Jupkwo <jupsfan@gmail.com>
2022-09-23 16:12:12 +0100

    Exception Handling

--- src/test/java/com/tutorialspoint/mockito/mathapplication/MathApplicationTest.java
+++ src/test/java/com/tutorialspoint/mockito/mathapplication/MathApplicationTest.java
@@ -1,10 +1,6 @@
 package com.tutorialspoint.mockito.mathapplication;
 
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.atMost;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doThrow;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -25,30 +21,12 @@ public class MathApplicationTest {
     @Mock
     CalculatorService calcService;
 
-    @Test
+    @Test(expected = RuntimeException.class)
     public void testAdd() {
-	// add the behavior of calc service to add two numbers
-	when(calcService.add(10.0, 20.0)).thenReturn(30.00);
-
-	// add the behavior of calc service to subtract two numbers
-	when(calcService.subtract(20.0, 10.0)).thenReturn(10.00);
+	// add the behavior to throw exception
+	doThrow(new RuntimeException("Add operation not implemented")).when(calcService).add(10.0, 20.0);
 
 	// test the add functionality
 	Assert.assertEquals(mathApplication.add(10.0, 20.0), 30.0, 0);
-	Assert.assertEquals(mathApplication.add(10.0, 20.0), 30.0, 0);
-	Assert.assertEquals(mathApplication.add(10.0, 20.0), 30.0, 0);
-
-	// test the subtract functionality
-	Assert.assertEquals(mathApplication.subtract(20.0, 10.0), 10.0, 0.0);
-
-	// check a minimum 1 call count
-	verify(calcService, atLeastOnce()).subtract(20.0, 10.0);
-
-	// check if add function is called minimum 2 times
-	verify(calcService, atLeast(2)).add(10.0, 20.0);
-
-	// check if add function is called maximum 3 times
-	verify(calcService, atMost(3)).add(10.0, 20.0);
-
     }
 }
