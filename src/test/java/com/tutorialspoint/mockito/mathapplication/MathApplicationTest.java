a14b3c6b5af023ee1f747d57cbb4f90d41d62035
Fanon Jupkwo <jupsfan@gmail.com>
2022-09-23 16:35:53 +0100

    ordered verification

--- src/test/java/com/tutorialspoint/mockito/mathapplication/MathApplicationTest.java
+++ src/test/java/com/tutorialspoint/mockito/mathapplication/MathApplicationTest.java
@@ -1,13 +1,14 @@
 package com.tutorialspoint.mockito.mathapplication;
 
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.junit.MockitoJUnitRunner;
 
 //@RunWith attaches a runner with the test class to initialize the test data
@@ -39,9 +40,12 @@ public class MathApplicationTest {
 	// test the add functionality
 	Assert.assertEquals(mathApplication.add(20.0, 10.0), 30.0, 0);
 
-	// verify call to calcService is made or not
+	// create an inOrder verifier for a single mock
+	InOrder inOrder = inOrder(calcService);
 
-	verify(calcService).subtract(20.0, 10.0);
-	verify(calcService).add(20.0, 10.0);
+	// following will make sure that add is first called then subtract is called.
+	inOrder.verify(calcService).add(20.0, 10.0);
+	inOrder.verify(calcService).subtract(20.0, 10.0);
+
     }
 }
