605c3aa01111a2226df7d11db7b7ce69ef684e80
Fanon Jupkwo <jupsfan@gmail.com>
2022-09-23 16:02:12 +0100

    expecting calls

--- src/test/java/com/tutorialspoint/mockito/mathapplication/MathApplicationTest.java
+++ src/test/java/com/tutorialspoint/mockito/mathapplication/MathApplicationTest.java
@@ -1,5 +1,7 @@
 package com.tutorialspoint.mockito.mathapplication;
 
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -27,10 +29,24 @@ public class MathApplicationTest {
 	// add the behavior of calc service to add two numbers
 	when(calcService.add(10.0, 20.0)).thenReturn(30.00);
 
+	// add the behavior of calc service to subtract two numbers
+	when(calcService.subtract(20.0, 10.0)).thenReturn(10.00);
 	// test the add functionality
 	Assert.assertEquals(mathApplication.add(10.0, 20.0), 30.0, 0);
+	Assert.assertEquals(mathApplication.add(10.0, 20.0), 30.0, 0);
+	Assert.assertEquals(mathApplication.add(10.0, 20.0), 30.0, 0);
 
-	// verify the behavior
-	verify(calcService).add(10.0, 20.0);
+	// test the subtract functionality
+	Assert.assertEquals(mathApplication.subtract(20.0, 10.0), 10.0, 0.0);
+
+	// default call count is 1
+	verify(calcService).subtract(20.0, 10.0);
+
+	// check if add function is called three times
+	verify(calcService, times(3)).add(10.0, 20.0);
+
+	// verify that method was never called on a mock
+	verify(calcService, never()).multiply(10.0, 20.0);
+
     }
 }
